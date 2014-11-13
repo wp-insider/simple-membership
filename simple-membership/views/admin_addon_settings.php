283fe5117e6644ef199247adcca709566fb23c03
wp.insider <>
2014-11-13 12:54:23 +1100

    Added a message in the addons settings tab v1.8.7 released to wp.org.

--- simple-membership/views/admin_addon_settings.php
+++ simple-membership/views/admin_addon_settings.php
@@ -2,6 +2,10 @@
 <h1><?= BUtils::_('Simple WP Membership::Settings')?></h1>
 <div class="wrap">
 <?php do_action("swpm-draw-tab"); ?>
+    
+    <p>
+        <?= BUtils::_("Some of the simple membership plugin's addon settings and options will be displayed here (if you have them)")?>
+    </p>
     <form action="" method="POST">
         <input type="hidden" name="tab" value="<?php echo $current_tab;?>" />
         <?php do_action('swpm_addon_settings_section');?>

014380675658dd50e633ce9f08a67040201f06ae
nur <nur858@gmail.com>
2015-01-10 01:27:30 -0500

    login form: converted to div

--- simple-membership/views/login.php
+++ simple-membership/views/login.php
@@ -1,37 +1,37 @@
 <div class="swpm-login-widget-form">
 <form id="swpm-login-form" name="swpm-login-form" method="post" action="">
-<table width="95%" border="0" cellpadding="3" cellspacing="5" class="forms">
-	    <tr>
-	    	<td colspan="2"><label for="swpm_user_name" class="eMember_label"><?= BUtils::_('User Name')?></label></td>
-	    </tr>
-	    <tr>
-	        <td colspan="2"><input type="text" class="swpm_text_field" id="swpm_user_name"  value="" size="30" name="swpm_user_name" /></td>
-	    </tr>
-	    <tr>
-	    	<td colspan="2"><label for="swpm_password" class="eMember_label"><?= BUtils::_('Password')?></label></td>
-		</tr>
-	    <tr>
-	        <td colspan="2"><input type="password" class="swpm_text_field" id="swpm_password" value="" size="30" name="swpm_password" /></td>
-	    </tr>
-	    <tr>
-	        <td colspan="2"><input type="checkbox" name="rememberme" value="checked='checked'"> <?= BUtils::_('Remember Me')?></td>
-	    </tr>
-	    <tr>
-	        <td colspan="2">
+    <div class="forms">
+	    <div>
+                <div ><label for="swpm_user_name" class="eMember_label"><?= BUtils::_('User Name')?></label></div>
+	    </div
+	    <div>
+	        <div ><input type="text" class="swpm_text_field" id="swpm_user_name"  value="" size="30" name="swpm_user_name" /></div>
+	    </div>
+	    <div>
+	    	<div ><label for="swpm_password" class="eMember_label"><?= BUtils::_('Password')?></label></div>
+		</div>
+	    <div>
+	        <div ><input type="password" class="swpm_text_field" id="swpm_password" value="" size="30" name="swpm_password" /></div>
+	    </div>
+	    <div>
+	        <div ><input type="checkbox" name="rememberme" value="checked='checked'"> <?= BUtils::_('Remember Me')?></div>
+	    </div>
+	    <div>
+	        <div >
                     <input type="submit" name="swpm-login" value="<?= BUtils::_('Login')?>"/>
-	        </td>
-	    </tr>
-        <tr>
-	        <td colspan="2">
+	        </div>
+	    </div>
+        <div>
+	        <div >
 	        <a id="forgot_pass" href="<?= $password_reset_url;?>"><?= BUtils::_('Forgot Password')?>?</a>
-	        </td>
-	    </tr>
-	    <tr>
-	        <td colspan="2"><a id="register" class="register_link" href="<?= $join_url; ?>"><?= BUtils::_('Join Us')?></a></td>
-	    </tr>
-	    <tr>
-	    	<td colspan="2"><span class="swpm-login-widget-action-msg"><?= $auth->get_message();?></span></td>
-	    </tr>
-	</table>
+	        </div>
+	    </div>
+	    <div>
+	        <div ><a id="register" class="register_link" href="<?= $join_url; ?>"><?= BUtils::_('Join Us')?></a></div>
+	    </div>
+	    <div>
+	    	<div ><span class="swpm-login-widget-action-msg"><?= $auth->get_message();?></span></div>
+	    </div>
+	</div>
 </form>
 </div>

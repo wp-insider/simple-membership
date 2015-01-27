fbd5225d62f72c1f122f7e5c8314b7941aa310f2
nur <nur858@gmail.com>
2015-01-26 21:10:04 -0500

    i18n

--- simple-membership/views/admin_edit_level.php
+++ simple-membership/views/admin_edit_level.php
@@ -18,21 +18,21 @@
         <th scope="row"><label for="subscription_period"><?= BUtils::_('Subscription Duration'); ?> <span class="description"><?= BUtils::_('(required)'); ?></span></label>
         </th>
         <td>
-                <p><input type="radio" <?= checked(BMembershipLevel::NO_EXPIRY,$subscription_duration_type,false)?> value="<?= BMembershipLevel::NO_EXPIRY?>" name="subscription_duration_type" /> No Expiry (Access for this level will not expire until cancelled)</p>                                
-                <p><input type="radio" <?= checked(BMembershipLevel::DAYS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::DAYS ?>" name="subscription_duration_type" /> Expire After 
-                    <input type="text" value="<?= checked(BMembershipLevel::DAYS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::DAYS ?>"> Days (Access expires after given number of days)</p>
+                <p><input type="radio" <?= checked(BMembershipLevel::NO_EXPIRY,$subscription_duration_type,false)?> value="<?= BMembershipLevel::NO_EXPIRY?>" name="subscription_duration_type" /> <?= BUtils::_('No Expiry (Access for this level will not expire until cancelled)')?></p>                                
+                <p><input type="radio" <?= checked(BMembershipLevel::DAYS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::DAYS ?>" name="subscription_duration_type" /> <?= BUtils::_('Expire After')?> 
+                    <input type="text" value="<?= checked(BMembershipLevel::DAYS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::DAYS ?>"> <?= BUtils::_('Days (Access expires after given number of days)')?></p>
                 
-                <p><input type="radio" <?= checked(BMembershipLevel::WEEKS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::WEEKS?>" name="subscription_duration_type" /> Expire After 
-                    <input type="text" value="<?= checked(BMembershipLevel::WEEKS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::WEEKS ?>"> Weeks (Access expires after given number of weeks)</p>
+                <p><input type="radio" <?= checked(BMembershipLevel::WEEKS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::WEEKS?>" name="subscription_duration_type" /> <?= BUtils::_('Expire After')?> 
+                    <input type="text" value="<?= checked(BMembershipLevel::WEEKS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::WEEKS ?>"> <?= BUtils::_('Weeks (Access expires after given number of weeks)')?></p>
                 
-                <p><input type="radio" <?= checked(BMembershipLevel::MONTHS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::MONTHS?>" name="subscription_duration_type" /> Expire After 
-                    <input type="text" value="<?= checked(BMembershipLevel::MONTHS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::MONTHS?>"> Months (Access expires after given number of months)</p>
+                <p><input type="radio" <?= checked(BMembershipLevel::MONTHS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::MONTHS?>" name="subscription_duration_type" /> <?= BUtils::_('Expire After')?> 
+                    <input type="text" value="<?= checked(BMembershipLevel::MONTHS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::MONTHS?>"> <?= BUtils::_('Months (Access expires after given number of months)')?></p>
                 
-                <p><input type="radio" <?= checked(BMembershipLevel::YEARS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::YEARS?>" name="subscription_duration_type" /> Expire After 
-                    <input type="text" value="<?= checked(BMembershipLevel::YEARS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::YEARS?>"> Years (Access expires after given number of years)</p>                
+                <p><input type="radio" <?= checked(BMembershipLevel::YEARS,$subscription_duration_type,false)?> value="<?= BMembershipLevel::YEARS?>" name="subscription_duration_type" /> <?= BUtils::_('Expire After')?> 
+                    <input type="text" value="<?= checked(BMembershipLevel::YEARS,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::YEARS?>"> <?= BUtils::_('Years (Access expires after given number of years)')?></p>                
                 
-                <p><input type="radio" <?= checked(BMembershipLevel::FIXED_DATE,$subscription_duration_type,false)?> value="<?= BMembershipLevel::FIXED_DATE?>" name="subscription_duration_type" /> Fixed Date Expiry 
-                    <input type="text" class="swpm-date-picker" value="<?= checked(BMembershipLevel::FIXED_DATE,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::FIXED_DATE?>" id="subscription_period_<?= BMembershipLevel::FIXED_DATE?>"> (Access expires on a fixed date)</p>                                
+                <p><input type="radio" <?= checked(BMembershipLevel::FIXED_DATE,$subscription_duration_type,false)?> value="<?= BMembershipLevel::FIXED_DATE?>" name="subscription_duration_type" /> <?= BUtils::_('Fixed Date Expiry')?> 
+                    <input type="text" class="swpm-date-picker" value="<?= checked(BMembershipLevel::FIXED_DATE,$subscription_duration_type,false)? $subscription_period: "";?>" name="subscription_period_<?= BMembershipLevel::FIXED_DATE?>" id="subscription_period_<?= BMembershipLevel::FIXED_DATE?>"> <?= BUtils::_('(Access expires on a fixed date)')?></p>                                
         </td>        
     </tr>
     <?php if(function_exists('swpm_protect_older_posts_addon')){ ?>
@@ -40,7 +40,7 @@
         <th scope="row"><label for="role"><?= BUtils::_('Protect Older Posts (optional)'); ?></span></label></th>
         <td>
             <input type="checkbox" <?= empty($protect_older_posts)? "": "checked='checked'"?>  value="1"  name="protect_older_posts" id="protect_older_posts" />
-            <p class="description">Only allow access to protected posts published after the members's join date.</p>
+            <p class="description"><?= BUtils::_('Only allow access to protected posts published after the members\'s join date.')?></p>
         </td>
     </tr>
     <?php } ?>

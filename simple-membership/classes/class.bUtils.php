70af9c63620cd889a1e864d0ea24d418c1512d36
amin0_000 <amin0_000@aminpc>
2015-03-15 20:31:03 +1100

    cosmetic changes for delete account profile option

--- simple-membership/classes/class.bUtils.php
+++ simple-membership/classes/class.bUtils.php
@@ -298,6 +298,6 @@ class BUtils {
         $allow_account_deletion = BSettings::get_instance()->get_value('allow-account-deletion');
         if (empty($allow_account_deletion)) {return "";}
         
-        return  '<a href="?delete_account=1"><div class="swpm-account-delete-button">' . BUtils::_("Delete Account") . '</div></a>';        
+        return  '<div class="swpm-account-delete-button"><a href="?delete_account=1">' . BUtils::_("Delete Account") . '</a></div>';        
     }
 }

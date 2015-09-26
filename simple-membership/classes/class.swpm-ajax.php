9ff1444066881b04d39cce5d67f52892617479c0
nur <nur858@gmail.com>
2015-09-25 23:49:01 -0400

    typo fix

--- simple-membership/classes/class.swpm-ajax.php
+++ simple-membership/classes/class.swpm-ajax.php
@@ -15,7 +15,7 @@ class SwpmAjax {
             exit;
         }
         $table = $wpdb->prefix . "swpm_members_tbl";
-        $query = $wpdb->prepare("SELECT member_id FROM $table WHERE email = %s AND AND user_name != ''", $field_value);
+        $query = $wpdb->prepare("SELECT member_id FROM $table WHERE email = %s AND user_name != ''", $field_value);
         $db_id = $wpdb->get_var($query) ;
         $exists = ($db_id > 0) && $db_id != $member_id;
         echo '[ "' . $field_id . (($exists) ? '",false, "&chi;&nbsp;'.SwpmUtils::_('Aready taken').'"]' : '",true, "&radic;&nbsp;Available"]');

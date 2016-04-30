3124d214af525215e9f3417a7dcd6f8bdba9a4dd
Jedi Knight <>
2016-04-30 17:09:24 +0600

    added a new filter so the login URL value can be overridden via an addon

--- simple-membership/classes/class.swpm-utils-misc.php
+++ simple-membership/classes/class.swpm-utils-misc.php
@@ -205,13 +205,20 @@ class SwpmMiscUtils {
     }
     
     public static function get_login_link() {
-        $login = SwpmSettings::get_instance()->get_value('login-page-url');
-        $joinus = SwpmSettings::get_instance()->get_value('join-us-page-url');
-        if (empty($login) || empty($joinus)) {
-            return '<span style="color:red;">Simple Membership is not configured correctly.'
+        $login_url = SwpmSettings::get_instance()->get_value('login-page-url');
+        $joinus_url = SwpmSettings::get_instance()->get_value('join-us-page-url');
+        if (empty($login_url) || empty($joinus_url)) {
+            return '<span style="color:red;">Simple Membership is not configured correctly. The login page or the join us page URL is missing in the settings configuration. '
                     . 'Please contact <a href="mailto:' . get_option('admin_email') . '">Admin</a>';
         }
-        return SwpmUtils::_('Please') . ' <a class="swpm-login-link" href="' . $login . '">' . SwpmUtils::_('Login') . '</a>. ' . SwpmUtils::_('Not a Member?') . ' <a href="' . $joinus . '">' . SwpmUtils::_('Join Us') . '</a>';
+        
+        //Create the login/protection message
+        $filtered_login_url = apply_filters('swpm_get_login_link_url', $login_url);//Addons can override the login URL value using this filter.
+        $login_msg = '';
+        $login_msg .= SwpmUtils::_('Please') . ' <a class="swpm-login-link" href="' . $filtered_login_url . '">' . SwpmUtils::_('Login') . '</a>. ';
+        $login_msg .= SwpmUtils::_('Not a Member?') . ' <a href="' . $joinus_url . '">' . SwpmUtils::_('Join Us') . '</a>';
+        
+        return $login_msg;
     }
 
     public static function get_renewal_link() {

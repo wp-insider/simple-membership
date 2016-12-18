1259654f2e24f7cd40b759c71feb728c77ea9498
Jedi knight <>
2016-12-18 18:35:47 +1100

    Added new utility function

--- simple-membership/classes/class.swpm-utils-membership-level.php
+++ simple-membership/classes/class.swpm-utils-membership-level.php
@@ -6,4 +6,19 @@
 
 class SwpmMembershipLevelUtils {
 
+    public static function get_membership_level_name_of_a_member($member_id){
+        $user_row = SwpmMemberUtils::get_user_by_id($member_id);
+        $level_id = $user_row->membership_level;
+        
+        $level_row = SwpmUtils::get_membership_level_row_by_id($level_id);
+        $level_name = $level_row->alias;
+        return $level_name;
+    }
+    
+    public static function get_membership_level_name_by_level_id($level_id){        
+        $level_row = SwpmUtils::get_membership_level_row_by_id($level_id);
+        $level_name = $level_row->alias;
+        return $level_name;
+    }
+    
 }

c86511f39166d3673327cda2e95d04c228a75190
Thomas Lejeune <thomas.lejeune@moovweb.com>
2020-09-02 20:11:33 +0300

    Added dynamic label for the Username, and fixed typy in the 'setting' variable name

--- simple-membership/views/login.php
+++ simple-membership/views/login.php
@@ -1,14 +1,17 @@
 <?php
 $auth = SwpmAuth::get_instance();
-$setting = SwpmSettings::get_instance();
-$password_reset_url = $setting->get_value('reset-page-url');
-$join_url = $setting->get_value('join-us-page-url');
+$settings = SwpmSettings::get_instance();
+$password_reset_url = $settings->get_value('reset-page-url');
+$join_url = $settings->get_value('join-us-page-url');
+
+// If the option use-automatic-username is enabled, set the username label to 'Email', if not, keep the usual 'Username or Email'
+$swpm_username_label  = !empty($settings->get_value('use-automatic-username')) ? 'Email' : 'Username or Email';
 ?>
 <div class="swpm-login-widget-form">
     <form id="swpm-login-form" name="swpm-login-form" method="post" action="">
         <div class="swpm-login-form-inner">
             <div class="swpm-username-label">
-                <label for="swpm_user_name" class="swpm-label"><?php echo SwpmUtils::_('Username or Email') ?></label>
+                <label for="swpm_user_name" class="swpm-label"><?php echo SwpmUtils::_($swpm_username_label) ?></label>
             </div>
             <div class="swpm-username-input">
                 <input type="text" class="swpm-text-field swpm-username-field" id="swpm_user_name" value="" size="25" name="swpm_user_name" />
@@ -23,9 +26,7 @@ $join_url = $setting->get_value('join-us-page-url');
                 <span class="swpm-remember-checkbox"><input type="checkbox" name="rememberme" value="checked='checked'"></span>
                 <span class="swpm-rember-label"> <?php echo SwpmUtils::_('Remember Me') ?></span>
             </div>
-            
             <div class="swpm-before-login-submit-section"><?php echo apply_filters('swpm_before_login_form_submit_button', ''); ?></div>
-            
             <div class="swpm-login-submit">
                 <input type="submit" class="swpm-login-form-submit" name="swpm-login" value="<?php echo SwpmUtils::_('Login') ?>"/>
             </div>

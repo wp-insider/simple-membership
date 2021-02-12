eb7f9e2e2e6c15846498fcc218b217317363a9c2
MedTR-git <13505192+MedTR-git@users.noreply.github.com>
2021-02-12 22:40:22 +0100

    Update admin_member_form_common_js.php
    Show button Panel which contains today's button.
    i think it's useful for manual membership renew

--- simple-membership/views/admin_member_form_common_js.php
+++ simple-membership/views/admin_member_form_common_js.php
@@ -1,6 +1,6 @@
 <script>
     jQuery(document).ready(function ($) {
-        $('#subscription_starts').datepicker({dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true, yearRange: "-100:+100"});
+        $('#subscription_starts').datepicker({dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true, yearRange: "-100:+100",showButtonPanel: true});
         $('#member_since').datepicker({dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true, yearRange: "-100:+100"});
     });
 </script>

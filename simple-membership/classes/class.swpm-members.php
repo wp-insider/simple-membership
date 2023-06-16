d739c5855cc5bd2766c7baab0c3086b24e7a5fc7
wp-insider <wp.insider.help@gmail.com>
2023-06-16 17:36:51 +1000

    translation related

--- simple-membership/classes/class.swpm-members.php
+++ simple-membership/classes/class.swpm-members.php
@@ -676,7 +676,7 @@ class SwpmMembers extends WP_List_Table {
 
 						<tr valign="top">
 							<td width="25%" align="left">
-								<strong>Access Starts Date: </strong>
+								<strong><?php _e( 'Access Starts Date: ', 'simple-membership' ); ?></strong>
 							</td><td align="left">
 								<input name="swpm_bulk_user_start_date_change_date" id="swpm_bulk_user_start_date_change_date" class="swpm-select-date" type="text" size="20" value="<?php echo ( date( 'Y-m-d' ) ); ?>" />
 								<p class="description"><?php _e( 'Specify the Access Starts date value.', 'simple-membership' ); ?></p>

6369187f8adae866a7626c073936d4c965b73d75
wp-insider <wp.insider.help@gmail.com>
2023-07-04 19:00:30 +1000

    cp2

--- simple-membership/lib/paypal/onboarding-related/class-swpm-paypal-onboarding.php
+++ simple-membership/lib/paypal/onboarding-related/class-swpm-paypal-onboarding.php
@@ -15,23 +15,43 @@ class SWPM_PayPal_PPCP_Onboarding {
 		//add_action( 'wp_loaded', array(&$this, 'handle_paypal_stuff' ) );
 	}
 
+	public static function generate_seller_nonce() {
+		// Generate a random string of 40 characters.
+		$random_string = substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 5)), 0, 40);
+
+		// Hash the string using sha256
+		$hashed_string = hash('sha256', $random_string);
+
+		// Trim or pad the hashed string to ensure it is between 40 to 64 characters
+		$output_string = substr($hashed_string, 0, 64);
+		$output_string = str_pad($output_string, 64, '0');
+
+		$seller_nonce = $output_string;
+		return $seller_nonce;
+	}
+
 	public static function get_sandbox_signup_link(){
-		$seller_nonce = wp_create_nonce('swpm_ppcp_sandbox_signup');
+
+		//TODO - Save the query args in options table (so the seller nonce and other details can be accessed easily after the onboarding process).
+		$seller_nonce = self::generate_seller_nonce();
 
 		$query_args = array();
 		$query_args['partnerId'] = self::$sandbox_partner_id;
-		$query_args['product'] = 'ppcp';
+		$query_args['product'] = 'PPCP';
 		$query_args['integrationType'] = 'FO';
 		$query_args['features'] = 'PAYMENT,REFUND';
 		$query_args['partnerClientId'] = self::$sandbox_partner_client_id;
-		$query_args['returnToPartnerUrl'] = '';
-		$query_args['partnerLogoUrl'] = '';
+		//$query_args['returnToPartnerUrl'] = '';
+		//$query_args['partnerLogoUrl'] = '';
 		$query_args['displayMode'] = 'minibrowser';
 		$query_args['sellerNonce'] = $seller_nonce;
 
 		$base_url = 'https://www.sandbox.paypal.com/bizsignup/partner/entry';
 		$sandbox_singup_link = add_query_arg( $query_args, $base_url );
 		//Example URL = 'https://www.sandbox.paypal.com/bizsignup/partner/entry?partnerId=USVAEAM3FR5E2&product=ppcp&integrationType=FO&features=PAYMENT,REFUND&partnerClientId=AeO65uHbDsjjFBdx3DO6wffuH2wIHHRDNiF5jmNgXOC8o3rRKkmCJnpmuGzvURwqpyIv-CUYH9cwiuhX&returnToPartnerUrl=&partnerLogoUrl=&displayMode=minibrowser&sellerNonce=a575ab0ee0';
+		
+		update_option('swpm_ppcp_sandbox_connect_query_args', $query_args);
+
 		return $sandbox_singup_link;
 	}
 
@@ -40,17 +60,20 @@ class SWPM_PayPal_PPCP_Onboarding {
 
 		?>
 		<script>
-			function swpm_ppcp_testmode_onboardedCallback(authCode, sharedId) {
-				console.log('SWPM PayPal Testmode onboardedCallback');
+			function swpm_ppcp_sandbox_onboardedCallback(authCode, sharedId) {
+				console.log('SWPM PayPal Sandbox onboardedCallback');
 				console.log(authCode);
 				console.log(sharedId);
 
 				//TODO - Send the authCode and sharedId to your server and do the next steps.
+				//The get_option('swpm_ppcp_sandbox_connect_query_args') will give you the query args that you sent to the PayPal onboarding page
+				//You can use the sellerNonce to identify the user.
 
+				
 			}
 		</script>
 		<a class="button button-primary direct" target="_blank"
-			data-paypal-onboard-complete="swpm_ppcp_testmode_onboardedCallback"
+			data-paypal-onboard-complete="swpm_ppcp_sandbox_onboardedCallback"
 			href="<?php echo esc_url_raw($sandbox_singup_link); ?>"
 			data-paypal-button="true">Activate PayPal Sandbox</a>
 		<script id="paypal-js" src="https://www.sandbox.paypal.com/webapps/merchantboarding/js/lib/lightbox/partner.js"></script>

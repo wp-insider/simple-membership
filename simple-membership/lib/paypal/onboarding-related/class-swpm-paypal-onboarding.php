455857e700ac61e8234231d9abe80c1e5032ad71
wp-insider <wp.insider.help@gmail.com>
2023-07-19 11:08:15 +1000

    test

--- simple-membership/lib/paypal/onboarding-related/class-swpm-paypal-onboarding.php
+++ simple-membership/lib/paypal/onboarding-related/class-swpm-paypal-onboarding.php
@@ -116,6 +116,7 @@ class SWPM_PayPal_PPCP_Onboarding {
 		<script id="paypal-js" src="https://www.sandbox.paypal.com/webapps/merchantboarding/js/lib/lightbox/partner.js"></script>
 
 		<?php
-
+		//Test
+		
 	}
 }

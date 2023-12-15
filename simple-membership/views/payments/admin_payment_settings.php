3696bf96a5fdb7d5d5792f5d4bad88fc6ee48cae
Jedi <support@tipsandtricks-hq.com>
2023-12-16 00:59:25 +1100

    minor text update

--- simple-membership/views/payments/admin_payment_settings.php
+++ simple-membership/views/payments/admin_payment_settings.php
@@ -4,10 +4,10 @@
 ?>
 <div class="swpm-yellow-box">
     <p>
-    <?php _e('The payment settings have moved to the <b>Payment Settings</b> tab in the <strong>Payments</strong> menu. Click the link below to navigate to the new page.','simple-membership');?>
+    <?php _e('The payment settings have moved to the <b>Payment Settings</b> tab in the <strong>Payments</strong> menu. Click the link below to navigate to the new location.','simple-membership');?>
     </p>
     <p>
-    <a class="button-primary" href="admin.php?page=simple_wp_membership_payments&tab=payment_settings"><?php _e('Go to the new settings menu','simple-membership');?></a>
+    <a class="button-primary" href="admin.php?page=simple_wp_membership_payments&tab=payment_settings"><?php _e('Go to the new Payment Settings location','simple-membership');?></a>
     </p>
 </div>
 <?php

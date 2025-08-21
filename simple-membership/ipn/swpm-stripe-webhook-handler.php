3e149f39298e7f35578ce505c28ff0b4bdb3f130
adilrabid <rabid1819@gmail.com>
2025-08-21 17:37:42 +0600

    small changes

--- simple-membership/ipn/swpm-stripe-webhook-handler.php
+++ simple-membership/ipn/swpm-stripe-webhook-handler.php
@@ -28,7 +28,7 @@ class SwpmStripeWebhookHandler {
 		$webhook_signing_secret = SwpmSettings::get_instance()->get_value( 'stripe-webhook-signing-secret' );
 		if ( ! empty( $webhook_signing_secret ) ) {
 			SwpmLog::log_simple_debug( 'Stripe webhook signing secret is configured. Validating this webhook event...', true );
-			$event_json = $this->validate_webhook_data( $input );
+			$event_json = self::validate_webhook_data( $input );
 			if ( empty( $event_json ) ) {
 				//Invalid webhook data received. Don't process this request.
 				http_response_code( 400 );
@@ -234,6 +234,54 @@ class SwpmStripeWebhookHandler {
 
 		return $charge_id;
 	}
+
+	public static function validate_webhook_data( $event_data_raw ){
+		$event_json = json_decode( $event_data_raw );
+
+		$sub_id = '';
+		if ( isset($event_json->data->object->subscription) ){
+			$sub_id = $event_json->data->object->subscription;
+		} else if (isset( $event_json->data->object->id ) && $event_json->data->object->object == 'subscription' ){
+			$sub_id = $event_json->data->object->id;
+		}
+
+		$sub_agreement_cpt_id = SWPM_Utils_Subscriptions::get_subscription_agreement_cpt_id_by_subs_id($sub_id);;
+
+		// Check if the sandbox mode is enabled
+		$sandbox_enabled = SwpmSettings::get_instance()->get_value( 'enable-sandbox-testing' );
+		$webhook_signing_secret = SwpmSettings::get_instance()->get_value( 'stripe-webhook-signing-secret' );
+
+		$payment_button_id = get_post_meta($sub_agreement_cpt_id, 'payment_button_id', true);
+
+		$api_keys = SwpmMiscUtils::get_stripe_api_keys_from_payment_button( $payment_button_id, !$sandbox_enabled );
+
+		if (!isset($api_keys['secret'])){
+			SwpmLog::log_simple_debug('Stripe API secret key could not be retrieved. Could not validate this webhook.', false);
+			return false;
+		}
+
+		// Include the Stripe library.
+		SwpmMiscUtils::load_stripe_lib();
+
+		\Stripe\Stripe::setApiKey( $api_keys['secret'] );
+
+		$stripe_signature_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+
+		try {
+			$event_json = \Stripe\Webhook::constructEvent($event_data_raw, $stripe_signature_header, $webhook_signing_secret);
+		} catch(\UnexpectedValueException $e) {
+			// Invalid payload. Don't Process this request.
+			SwpmLog::log_simple_debug('Error parsing payload: ' . $e->getMessage() , false);
+			return false;
+		} catch(\Stripe\Exception\SignatureVerificationException $e) {
+			// Invalid signature. Don't Process this request.
+			SwpmLog::log_simple_debug('Error verifying webhook signature: ' . $e->getMessage() , false);
+			return false;
+		}
+
+		return $event_json;
+	}
+
 }
 
 new SwpmStripeWebhookHandler();

18f20769a0715765d2d8c72f0574d8d3c24ca857
adilrabid <rabid1819@gmail.com>
2025-09-14 18:41:26 +0600

    hook updated

--- simple-membership/views/login.php
+++ simple-membership/views/login.php
@@ -78,5 +78,5 @@ $hide_join_us_link_enabled = SwpmSettings::get_instance()->get_value('hide-join-
         </div>
     </form>
 
-    <?php do_action('swpm_after_login_form'); ?>
+    <?php echo apply_filters('swpm_after_login_form_output', ''); ?>
 </div>
